2b3715366dac72137d4585ffae8b19264221765c
dredhorse <dredhorse@breiden.net>
2012-04-30 10:47:46 +0200

    fixed some issues with metrics , v 2.8

--- src/org/simiancage/bukkit/messagechangerlite/Metrics.java
+++ src/org/simiancage/bukkit/messagechangerlite/Metrics.java
@@ -93,7 +93,7 @@ public class Metrics {
 	/**
 	 * The base url of the metrics domain
 	 */
-	private static final String BASE_URL = "http://metrics.griefcraft.com";
+	private static final String BASE_URL = "http://mcstats.org/";
 
 	/**
 	 * The url used to report a server's status
